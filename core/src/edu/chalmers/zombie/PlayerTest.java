e5dff9696179e5d0d5d81bf98b4cce4934345141
oherik <erik.ohrn@outlook.com>
2015-03-30 16:38:59 +0200

    Started on player framework

--- core/src/edu/chalmers/zombie/PlayerTest.java
+++ core/src/edu/chalmers/zombie/PlayerTest.java
@@ -1,11 +1,32 @@
 package edu.chalmers.zombie;
 
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 /**
  * Created by Erik on 2015-03-30.
  */
 public class PlayerTest extends Sprite {
 
-    
+    private Vector2 v;  //velocity
+    private float s; //speed
+
+    public PlayerTest(Sprite sprite){
+        super(sprite);
+        v = new Vector2(0,0);
+        s = 64;
+    }
+
+    @Override
+    public void draw(Batch batch){
+
+    }
+
+    public void updatePlayer(float f){
+        
+    }
+
+
 }
