ddd21bf55811c2fa643bd32ba7d752c5149fc538
oherik <erik.ohrn@outlook.com>
2015-04-01 16:31:10 +0200

    Created a first version of the path finding algorithm

--- core/src/Model/PathAlgorithm.java
+++ core/src/Model/PathAlgorithm.java
@@ -1,103 +1,84 @@
 package Model;
 
 import java.awt.*;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.PriorityQueue;
-import java.util.Set;
-import com.badlogic.gdx.maps.tiled.TiledMap;
+import java.util.*;
+
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 
 /**
  * Created by Erik on 2015-04-01.
  * This is the class which handles the path finding for the zombies. It uses A*, which is a modified version of Dijkstra's algorithm.
  */
 public class PathAlgorithm {
-    private TiledMap map;
+    private TiledMapTileLayer emptyTiles;
     private Point startPos, endPos;
 
     /**
      * Constructor
-     * @param map   The current map in which the player and the zombie are
+     *
+     * @param emptyTiles The current map in which the player and the zombie are
      */
 
-    public PathAlgorithm(TiledMap map){
-        this.map = map;
+    public PathAlgorithm(TiledMapTileLayer emptyTiles) {
+        this.emptyTiles = this.emptyTiles;
     }
 
     /**
-    * @param startPos  The starting position (normally the zombie's position)
-    * @param endPos    The end position (normally the player position)
-    */
+     * @param startPos The starting position (normally the zombie's position)
+     * @param endPos   The end position (normally the player position)
+     */
 
-    public Iterator<Point> getPath(Point startPos, Point endPos){
+    public Iterator<Point> getPath(Point startPos, Point endPos) {
         this.startPos = startPos;
         this.endPos = endPos;
         return calculatePath();
     }
 
+    /**
+     * Calculates the shortest path given the data from the constructor
+     *
+     * @return the shortest path or, if none found, null
+     */
 
-    private Iterator<Point> calculatePath(){
-        PriorityQueue<QueueElement> queue;
-        
+    private Iterator<Point> calculatePath() {
+        PriorityQueue<QueueElement> queue = new PriorityQueue<QueueElement>();
+        ArrayList<Point> path;
+        QueueElement currentElement;
+        int width = emptyTiles.getWidth();
+        int height = emptyTiles.getHeight();
+        boolean[][] closedNodes = new boolean[width][height];
 
+        queue.add(new QueueElement(this.startPos, 0, new ArrayList<Point>()));
 
-    }
+        while (!queue.isEmpty()) {
+            currentElement = queue.poll();
+            Point currentNode = currentElement.getNode();
+            int x = currentNode.x;
+            int y = currentNode.y;
 
-
-
-
-
-}
-
-    public Iterator<E> shortestPath(int from, int to) {
-        if (from < 0)
-            throw new IndexOutOfBoundsException(
-                    "DirectedGraph - shortestPath(int from, int to): from f?r inte vara negativ");
-        else if (to < 0)
-            throw new IndexOutOfBoundsException(
-                    "DirectedGraph - shortestPath(int from, int to): to f?r inte vara negativ");
-        // Variabeldeklaration
-        PriorityQueue<CompDijkstraPath<E>> dijkstraQueue = new PriorityQueue<CompDijkstraPath<E>>();
-        ArrayList<E> currentPath;
-        CompDijkstraPath<E> currentElement;
-        Set<Integer> visitedNodes = new HashSet<Integer>();
-
-        dijkstraQueue.add(new CompDijkstraPath<E>(from, 0, new ArrayList<E>())); // L?gg
-        // till
-        // startnoden
-        while (!dijkstraQueue.isEmpty()) {
-            currentElement = dijkstraQueue.poll(); // H?mta elementet med
-            // kortast v?g
-            int currentNode = currentElement.getNode();
-            if (!visitedNodes.contains(currentNode)) {
-                if (currentNode == to)
-                    return currentElement.getPath().iterator(); // Har kommit
-                    // fram,
-                    // returnera
-                    // v?gen dit
-                else {
-                    visitedNodes.add(currentNode);
-                    for (E e : edgeListArray[currentNode]) {
-                        if (!visitedNodes.contains(e.getDest())) {
-                            currentPath = new ArrayList<E>(
-                                    currentElement.getPath()); // Kopiera v?gen
-                            // fr?n
-                            // elementet
-                            // till en ny
-                            // lista
-                            currentPath.add(e); // Och l?gg till den aktuella
-                            // b?gen
-                            dijkstraQueue.add(new CompDijkstraPath<E>(e
-                                    .getDest(), e.getWeight()
-                                    + currentElement.getCost(), currentPath)); // L?gg
-                            // till
-                            // k?elementet
-                            // i
-                            // prioritetsk?n
+            if (currentNode == this.endPos)
+                return currentElement.getPath().iterator();
+            else {
+                for (int i = Math.max(0, x - 1); i <= Math.min(width, x + 1); i++) {
+                    for (int j = Math.max(0, y - 1); j <= Math.min(height, y + 1); j++) {
+                        if (!closedNodes[i][j] == true && emptyTiles.getCell(i, j) != null) {//i.e. if it's null or false and is walkable
+                            int h = 10 * (Math.abs(endPos.x - i) + Math.abs(endPos.y - j));     //Manhattan distance
+                            double g;
+                            if ((j == x - 1 && i == y - 1) || (j == x - 1 && i == y + 1) || (j == x + 1 && i == y - 1) || (j == x + 1 && i == y + 1)) {       //Diagonal
+                                g = currentElement.getCost() + 14;  // 10 * sqrt(2) is approx. 14
+                            } else {
+                                g = currentElement.getCost() + 10;
+                            }
+                            double cost = h + g;
+                            ArrayList<Point> currentPath = new ArrayList<Point>(currentElement.getPath());
+                            currentPath.add(currentNode);
+                            queue.add(new QueueElement(new Point(i, j), cost, currentPath));
                         }
                     }
                 }
             }
+
         }
-        return null; // Ingen v?g funnen
-    }
+        return null;    //No path found
+    }//calculatePath
+}
