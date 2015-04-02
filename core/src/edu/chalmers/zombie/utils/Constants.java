110c42838ac43c17e8a106adc880528742f30db1
Tobias <toobew@users.noreply.github.com>
2015-04-02 16:33:34 +0200

    created Constants and GameScreen

---
+++ core/src/edu/chalmers/zombie/utils/Constants.java
@@ -0,0 +1,9 @@
+package edu.chalmers.zombie.utils;
+
+/**
+ * Created by Tobias on 15-04-02.
+ */
+public class Constants {
+
+    public static final int TILE_SIZE = 32;
+}
---
+++ core/src/edu/chalmers/zombie/view/GameScreen.java
@@ -0,0 +1,6 @@
+package edu.chalmers.zombie.view;
+
+/**
+ * Created by Tobias on 15-04-02.
+ */
+public class GameScreen {}
