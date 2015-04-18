5347f38997625d9595041a508c698888c55b11ee
Erik Öhrn <erik.ohrn@outlook.com>
2015-04-18 09:58:27 +0200

    Added a level class

---
+++ core/src/edu/chalmers/zombie/Model/Level.java
@@ -0,0 +1,45 @@
+package edu.chalmers.zombie.model;
+
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+
+import java.io.FileNotFoundException;
+
+/**
+ * The general model for storing a specific level. The level contains a tiled map, which is the graphical representation of the level, and a box2d World which handles the physics.
+ */
+
+
+public class Level {
+    private World world;
+    private TiledMap tiledMap;
+
+    /**
+     * The constructor
+     * @param mapPath   The file path to the map
+     */
+    public Level(String mapPath){
+        if(mapPath == null)
+            throw new NullPointerException("Level: no path name recieved");
+        tiledMap = new TmxMapLoader().load(mapPath);
+        if(tiledMap == null)
+            throw new NullPointerException("Level: incorrect path name");
+        world = new World(new Vector2(), true);
+    }
+
+    /**
+     * @return the level's map
+     */
+    public TiledMap getMap(){
+        return this.tiledMap;
+    }
+
+    /**
+     * @return the level's box2d World
+     */
+    public World getWorld(){
+        return this.world;
+    }
+}
