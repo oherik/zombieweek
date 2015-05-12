71d65443ed4c7c4d3716b1073e30ccd02a5405d7
Neda Farhand <neda.farhand@gmail.com>
2015-05-12 15:33:11 +0200

    Added javadoc.

--- core/src/edu/chalmers/zombie/utils/ZombieType.java
+++ core/src/edu/chalmers/zombie/utils/ZombieType.java
@@ -1,6 +1,7 @@
 package edu.chalmers.zombie.utils;
 
 /**
+ * An Enum containing all available zombie types, which are used to determine a zombies properties. 
  * Created by neda on 2015-05-06.
  */
 public enum ZombieType {
