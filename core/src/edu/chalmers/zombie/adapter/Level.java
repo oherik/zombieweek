a78eac569377a8b2f3ad548e50b8cfb4cfd42dbf
Erik Öhrn <erik.ohrn@outlook.com>
2015-05-12 17:17:18 +0200

    Added javadoc to level

--- core/src/edu/chalmers/zombie/adapter/Level.java
+++ core/src/edu/chalmers/zombie/adapter/Level.java
@@ -1,14 +1,11 @@
 package edu.chalmers.zombie.adapter;
 
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapImageLayer;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.World;
 import edu.chalmers.zombie.controller.ContactListener;
 import edu.chalmers.zombie.utils.Constants;
@@ -17,24 +14,22 @@ import java.awt.*;
 import java.util.ArrayList;
 
 /**
- * The general model for storing a specific level. The level contains a tiled map, which is the graphical representation of the level, and a box2d World which handles the physics.
+ * The general model for storing a specific level. The level contains a tiled map, which is the graphical representation
+ * of the level and its meta data, and a box2d World which handles the physics. It also stores the zombies for the current
+ * level.
  */
-
-
 public class Level {
     private World world;
     private TiledMap tiledMap;
-    private float tileSize;
     private ArrayList zombies;
-    private Body ground;
-    private Sprite mapPainting, mapPaintingTopLayer;
     private boolean initializedBodies;
     Point playerSpawn, playerReturn;
     TiledMapTileLayer metaLayer;
     TiledMapImageLayer topLayer, bottomLayer;
+    
     /**
      * Creates a new level based on a tiled map and a Box2D world
-     * @param mapPath  The file path to the map containing the meta data
+     * @param mapPath  The file path to the map
      * @throws NullPointerException if the path name is incorrect or not found
      */
     public Level(String mapPath){
@@ -48,25 +43,32 @@ public class Level {
         metaLayer = (TiledMapTileLayer) tiledMap.getLayers().get(Constants.META_LAYER);
         topLayer = (TiledMapImageLayer) tiledMap.getLayers().get(Constants.TOP_LAYER);
         bottomLayer = (TiledMapImageLayer) tiledMap.getLayers().get(Constants.BOTTOM_LAYER);
-        tileSize = (float) Constants.TILE_SIZE;
 
         //Create the world
         world = new World(new Vector2(0,0), true);
         world.setContactListener(new ContactListener());
         initializedBodies = false;
 
-
         zombies = new ArrayList<Zombie>();
     }
 
+    /**
+     * @return The map's meta data layer
+     */
     public TiledMapTileLayer getMetaLayer(){
         return this.metaLayer;
     }
 
+    /**
+     * @return The map's top graphical layer
+     */
     public TiledMapImageLayer getTopLayer(){
         return this.topLayer;
     }
 
+    /**
+     * @return The map's bottom graphical layer
+     */
     public TiledMapImageLayer getBottomLayer(){
         return this.bottomLayer;
     }
@@ -94,7 +96,6 @@ public class Level {
         z.dispose();
         zombies.remove(z);
     }
-
 
     /**
      * @return the level's map
@@ -124,42 +125,50 @@ public class Level {
      */
     public void destroyBody(Body b){
         getWorld().destroyBody(b);
-        b=null;
+        b=null; //To avoid null pointer exceptions
     }
 
     /**
-     * @return The map painting
+     * Sets a variable which is true if the bodies (collision objects) have been initialized
+     * @param bool  true if the collision objects have been initialized, false if not
      */
-    public Sprite getMapPainting(){
-        return mapPainting;
-    }
-    /**
-     * @return The map painting to be rendered on top of the player and other entities
-     */
-    public Sprite getMapPaintingTopLayer(){
-        return mapPaintingTopLayer;
-    }
-
     public void setInitializedBodies(boolean bool){
         this.initializedBodies = bool;
     }
 
+    /**
+     * @return true if the collision objects have been initialized, false if not
+     */
     public boolean hasInitializedBodies(){
         return this.initializedBodies;
     }
 
+    /**
+     * @return The spawn point for the player
+     */
     public Point getPlayerSpawn(){
         return this.playerSpawn;
     }
 
+    /**
+     * @return The point where the player shall be placed if she returns to this level
+     */
     public Point getPlayerReturn(){
         return this.playerReturn;
     }
 
+    /**
+     * Sets where the player will spawn
+     * @param playerSpawn   A point on the map where the player will spawn
+     */
     public void setPlayerSpawn(Point playerSpawn){
         this.playerSpawn = playerSpawn;
     }
 
+    /**
+     * Sets where the player will be placed if she returns to this level
+     * @param playerReturn   A point on the map where the player will be placed if she returns to this level
+     */
     public void setPlayerReturn(Point playerReturn){
         this.playerReturn = playerReturn;
     }
