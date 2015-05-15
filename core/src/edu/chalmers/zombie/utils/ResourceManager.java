c4b941f4b3faf7799f72e6f3196a2664277d17da
Tobias <toobew@users.noreply.github.com>
2015-05-15 16:43:11 +0200

    Support for TiledMap added

--- core/src/edu/chalmers/zombie/utils/ResourceManager.java
+++ core/src/edu/chalmers/zombie/utils/ResourceManager.java
@@ -2,6 +2,9 @@ package edu.chalmers.zombie.utils;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+
 import java.util.HashMap;
 
 /**
@@ -12,21 +15,72 @@ import java.util.HashMap;
 public class ResourceManager {
 
     //Sprites?
-    //Maps?
     private HashMap<String, Texture> textureMap;
+    private HashMap<String, TiledMap> tiledmapMap;
 
+    /**
+     * Initializes the hashmaps
+     */
     public ResourceManager(){
         textureMap = new HashMap<String, Texture>();
+        tiledmapMap = new HashMap<String, TiledMap>();
+    }
+
+
+    /**
+     * Save tiled map using path and key
+     * @param key The key
+     * @param path The path to the tiled map
+     * @throws NullPointerException if the path is incorrect or not found
+     */
+    public void loadTiledMap(String key, String path){
+        if (path == null) {
+            throw new NullPointerException("Tiled map: no path recieved");
+        }
+
+        TiledMap tiledMap = new TmxMapLoader().load(path);
+
+        if (tiledMap == null) {
+            throw new NullPointerException("Tiled map: incorrect path name");
+        }
+
+        tiledmapMap.put(key,tiledMap);
     }
 
     /**
-     * Save texture with path and key
+     * Get tiled map
+     * @param key The tiled map
+     * @return tiled map
+     */
+    public TiledMap getTiledMap(String key){ return tiledmapMap.get(key);}
+
+    /**
+     * Dispose tiled map
+     * @param key The tiled map key
+     */
+    public void disposeTiledMap(String key){
+        TiledMap tiledMap = tiledmapMap.get(key);
+        if (tiledMap!=null){tiledMap.dispose();}
+    }
+
+    /**
+     * Save texture using path and key
      * @param key The key
      * @param path The path to the texture
+     * @throws NullPointerException if the path is incorrect or not found
      */
     public void loadTexture(String key, String path){
+        if (path == null) {
+            throw new NullPointerException("Texture: no path recieved");
+        }
+
         //Texture texture = new Texture(Gdx.files.internal(path));
         Texture texture = new Texture(path);
+
+        if (texture == null) {
+            throw new NullPointerException("Texture: incorrect path name");
+        }
+
         textureMap.put(key,texture);
     }
 
@@ -47,6 +101,8 @@ public class ResourceManager {
         Texture texture = textureMap.get(key);
         if (texture!=null){texture.dispose();}
     }
+
+
 
 
 
