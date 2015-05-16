3e7bd51c41c76625334b4e325e47bfaf063d78a6
Tobias <toobew@users.noreply.github.com>
2015-05-16 12:02:35 +0200

    Created enum for the state of the game

---
+++ core/src/edu/chalmers/zombie/utils/GameState.java
@@ -0,0 +1,12 @@
+package edu.chalmers.zombie.utils;
+
+/**
+ * Enum to represent the current state of the game
+ *
+ * Created by Tobias on 15-05-16.
+ */
+public enum GameState {
+    GAME_RUNNING,
+    GAME_PAUSED,
+    GAME_GAMEOVER
+}
