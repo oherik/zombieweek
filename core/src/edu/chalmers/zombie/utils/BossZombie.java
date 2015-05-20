d4ef95a28cde30cf24f313af597615bc8524b505
Neda Farhand <neda.farhand@gmail.com>
2015-05-20 11:19:22 +0200

    Created class BossZombie and begun implementation.

---
+++ core/src/edu/chalmers/zombie/utils/BossZombie.java
@@ -0,0 +1,47 @@
+package edu.chalmers.zombie.utils;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import edu.chalmers.zombie.adapter.Zombie;
+
+import java.awt.*;
+
+/**
+ * Created by neda on 2015-05-20.
+ */
+public class BossZombie extends Zombie {
+
+    private Sprite sprite;
+    //private int hp;
+    private World world;
+    private Point position;
+
+    public BossZombie(Sprite sprite, World world, int x, int y) {
+
+        super(sprite, world, x, y);
+        setType(ZombieType.BOSS);
+        this.world = world;
+        this.sprite = sprite;
+        position = new Point(x, y);
+        setDetectionRadius(10);
+        setStartingHp(200);
+        setSpeed(100);
+    }
+
+    @Override
+    public void attack() {
+
+    }
+
+    @Override
+    public Zombie spawn(World world, int x, int y) {
+
+        return new BossZombie(sprite, world, x, y);
+    }
+
+    @Override
+    public Vector2 getVelocity() {
+        return null;
+    }
+}
