da01530b334cf69e1b1a9ddee1a9194eebb95023
Erik Öhrn <erik.ohrn@outlook.com>
2015-05-24 16:49:24 +0200

    Moved functionality from ContactListener to EnityController

--- core/src/edu/chalmers/zombie/Controller/EntityController.java
+++ core/src/edu/chalmers/zombie/Controller/EntityController.java
@@ -33,6 +33,16 @@ public class EntityController {
 
     /* ---------------- PLAYER --------------------*/
 
+    /**
+     * @return  The current player
+     */
+    public static Player getPlayer(){
+        return GameModel.getInstance().getPlayer();
+    }
+    /**
+     * Sets the current player
+     * @param player The new player
+     */
     public static void setPlayer(Player player){
         GameModel.getInstance().setPlayer(player);
     }
@@ -53,6 +63,63 @@ public class EntityController {
         Player player = new Player(res.getTexture("emilia"), mapController.getWorld(), position.x, position.y);
         setPlayer(player); //TODO test);
         return player;
+    }
+
+    /**
+     * Increse the number of sneak tiles the player is touching by one. The player will start to sneak.
+     * @param player    The player
+     */
+    public static void increaseSneakTilesTouching(Player player){
+        if(player.getSneakTilesTouching()<0) {
+            player.setSneakTilesTouching(0);        //Negative numbers for tiles youching wouldn't make sense
+        }
+        player.setSneakTilesTouching(player.getSneakTilesTouching() + 1);
+        //TODO add more sneak stuff
+        player.setHidden(true); //TODO Nödvändigt? Zombien kommer ändå inte kunna hitta till spelaren
+        setFriction(player, Constants.PLAYER_FRICTION_SNEAK, Constants.PLAYER_FRICTION_SNEAK);  //TODO onödigt att göra varje gång?
+    }
+
+    /**
+     * Decrease the number of sneak tiles the player is touching by one. The player will stop to sneak if the total number of sneak tiles it's touching is 0.
+     * @param player The player
+     */
+    public static void decreaseSneakTilesTouching(Player player){
+        player.setSneakTilesTouching(player.getSneakTilesTouching() - 1);
+        if(player.getSneakTilesTouching()<1){
+            //TODO Sluta sneaka
+            player.setHidden(false);
+            EntityController.setFriction(player, Constants.PLAYER_FRICTION_DEFAULT, Constants.PLAYER_FRICTION_DEFAULT);
+            if(player.getSneakTilesTouching()<0) {
+                player.setSneakTilesTouching(0);        //Negative numbers for tiles touching wouldn't make sense
+            }
+        }
+    }
+
+    /**
+     * Increse the number of water tiles the player is touching by one. The player will slow down.
+     * @param player    The player
+     */
+    public static void increaseWaterTilesTouching(Player player){
+        if(player.getWaterTilesTouching()<0) {
+            player.setWaterTilesTouching(0);
+        }
+        player.setWaterTilesTouching(player.getWaterTilesTouching() + 1);
+            //TODO add water stuff
+        setFriction(player, Constants.PLAYER_FRICTION_WATER, Constants.PLAYER_FRICTION_WATER);
+    }
+
+    /**
+     * Decrease the number of water tiles the player is touching by one. The player will get out of the water if the total number of water tiles it's touching is 0.
+     * @param player The player
+     */
+    public static void decreaseWaterTilesTouching(Player player){
+        player.setWaterTilesTouching(player.getWaterTilesTouching() - 1);
+        if(player.getWaterTilesTouching()<1){
+            EntityController.setFriction(player, Constants.PLAYER_FRICTION_DEFAULT, Constants.PLAYER_FRICTION_DEFAULT);
+            if(player.getWaterTilesTouching()<0) {
+                player.setWaterTilesTouching(0);
+            }
+        }
     }
 
 
