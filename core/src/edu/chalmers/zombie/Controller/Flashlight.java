3faffcfe58f9ba2da606ef673539760fa79ff37d
thunderskillz <danieljohansson_95@hotmail.com>
2015-05-24 20:44:20 +0200

    flashlight stuff

--- core/src/edu/chalmers/zombie/Controller/Flashlight.java
+++ core/src/edu/chalmers/zombie/Controller/Flashlight.java
@@ -8,181 +8,120 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.EarClippingTriangulator;
-import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Fixture;
-import com.badlogic.gdx.physics.box2d.RayCastCallback;
-import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.ShortArray;
 import edu.chalmers.zombie.model.GameModel;
 import edu.chalmers.zombie.utils.Constants;
-
 import java.util.ArrayList;
 
 public class Flashlight {
-    private Vector2 playerPosition = new Vector2();
-    private Vector2 windowSize = new Vector2();
-    private ArrayList<Float> vertices = new ArrayList<Float>();
     private World world;
-    private Texture darkness = new Texture("core/assets/darkness.png");
-    private Texture light = new Texture("core/assets/light.png");
-    private boolean foundFixture;
+    private Texture darkTexture = new Texture("core/assets/darkness.png");
+    private Texture lightTexture = new Texture("core/assets/light.png");
+    private Vector2 playerPosition = new Vector2(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+    Vector2 collisionPoint = new Vector2();
     private float currentFraction = 1337;
-    private Vector2 collisionPoint = new Vector2();
+    private boolean foundFixture;
+    private ArrayList<Float> collisionPoints = new ArrayList<Float>();
+    private ArrayList<Float> corners = new ArrayList<Float>();
+    private float direction;
     private RayCastCallback callback = createCallback();
-    private float width;
-    private float length;
-    private int precision;
-    private Vector2[] rays;
-    private ArrayList<Vector2> endPoints = new ArrayList<Vector2>();
-    private Sprite lightRegion;
-    private TextureRegion darkRegion;
 
-
-    public Flashlight(World world) {
+    public Flashlight(World world){
         this.world = world;
-        width = Constants.PI/4;
-        length = 8;
-        precision = 20;
-        initializeRays();
-        initializeImages();
     }
 
-    public void draw(PolygonSpriteBatch polygonSpriteBatch, SpriteBatch spriteBatch){
-        clearAll();
-        fetchAllInfo();
-        lightRegion.setAlpha(0.15f);
-        lightRegion.draw(spriteBatch);
-        PolygonRegion darkPolygonRegion = createPolygonRegion();
-        polygonSpriteBatch.draw(createPolygonRegion(), 0, 0);
+    private void fetchDirection(){
+        GameModel gameModel = GameModel.getInstance();
+        direction = gameModel.getPlayer().getHand().getDirection() + Constants.PI/2;
     }
-
-    private void clearAll(){
-        vertices.clear();
-        endPoints.clear();
-    }
-
-    private void fetchAllInfo(){
-        fetchWindowSize();
-        fetchStandardVertices();
-        calculateEndPoints();
-        calculateCollisionPoints();
-    }
-
-    private void fetchStandardVertices(){
-        fetchPlayerPosition();
-        fetchCorners();
-
-    }
-
-    private void fetchWindowSize(){
-        windowSize.set(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-    }
-
     private void fetchPlayerPosition(){
         GameModel gameModel = GameModel.getInstance();
-        playerPosition =  new Vector2(gameModel.getPlayer().getX(), gameModel.getPlayer().getY());
-        vertices.add(playerPosition.x);
-        vertices.add(playerPosition.y);
+        playerPosition.set(gameModel.getPlayer().getX(), gameModel.getPlayer().getY());
     }
 
-    private void fetchCorners(){
-        vertices.add(playerPosition.x - windowSize.x/64);
-        vertices.add(windowSize.y/32 + playerPosition.y - windowSize.y/64);        //Top left
-        vertices.add(playerPosition.x - windowSize.x/64);
-        vertices.add(playerPosition.y - windowSize.y/64);                           //Bottom left
-        vertices.add(windowSize.x/32+playerPosition.x - windowSize.x/64);
-        vertices.add(playerPosition.y - windowSize.y/64);           //Bottom right
-        vertices.add(windowSize.x/32 + playerPosition.x - windowSize.x/64);
-        vertices.add(windowSize.y/32 + playerPosition.y - windowSize.y/64); //Top right
-    }
-
-    private void calculateEndPoints(){
-        for(int i = 0; i<precision; i++){
-            rays[i] = new Vector2(1,1);
-            rays[i].setLength(length);
-            rays[i].setAngleRad(getDirection() - width / 2 + i * width / precision);
+    public void draw(PolygonSpriteBatch psb, SpriteBatch sb){
+        GameModel gameModel = GameModel.getInstance();
+        fetchDirection();
+        fetchPlayerPosition();
+        float coneWidth = Constants.PI/4;
+        int numberOfRays = 100;
+        int coneLength = 8;
+        Vector2[] rays = new Vector2[numberOfRays];
+        Vector2 playerPosition =  new Vector2(gameModel.getPlayer().getX(), gameModel.getPlayer().getY());
+        ArrayList<Vector2> endPoints = new ArrayList<Vector2>();
+        for(int i = 0; i<numberOfRays; i++) {
+            rays[i] = new Vector2(1, 1);
+            rays[i].setLength(coneLength);
+            rays[i].setAngleRad(direction - coneWidth / 2 + i * coneWidth / numberOfRays);
             Vector2 end = new Vector2(rays[i]);
             end.add(playerPosition);
             endPoints.add(end);
+            int x = 0;
         }
-    }
-
-    private void calculateCollisionPoints(){
-        rayCast();
-    }
-
-    private float getDirection(){
-        GameModel gameModel = GameModel.getInstance();
-        return gameModel.getPlayer().getHand().getDirection();
-    }
-
-    private void rayCast(){
-        for (Vector2 line: rays){
+        for (Vector2 line : rays) {
             currentFraction = 1337;
             foundFixture = false;
-            world.rayCast(callback, playerPosition, new Vector2(line.x + playerPosition.x, line.y + playerPosition.y));
-            if (foundFixture){
+            world.rayCast(callback, new Vector2(gameModel.getPlayer().getX(), gameModel.getPlayer().getY()),
+                    new Vector2(line.x + gameModel.getPlayer().getX(), line.y + gameModel.getPlayer().getY()));
+            if (foundFixture) {
                 Vector2 temp = new Vector2(line);
                 temp.add(playerPosition);
                 int tempIndex = endPoints.indexOf(temp);
                 endPoints.remove(temp);
-                endPoints.add(tempIndex,new Vector2(collisionPoint));
+                endPoints.add(tempIndex, new Vector2(collisionPoint));
+            }
+
+        }
+        collisionPoints.clear();
+        endPoints.add(new Vector2(gameModel.getPlayer().getX(), gameModel.getPlayer().getY()));
+        float maxY = 0;
+        int maxYIndex = -1;
+        for(int i = 0; i<endPoints.size(); i++){
+            float currentY = endPoints.get(i).y;
+            if(currentY>maxY){
+                maxY = currentY;
+                maxYIndex = i;
             }
         }
-        vertices.addAll(convert(endPoints));
-    }
 
-    private ArrayList<Float> convert(ArrayList<Vector2> vectorList){
-        ArrayList<Float> returnList = new ArrayList<Float>();
-        for(Vector2 v: vectorList){
-            returnList.add(v.x);
-            returnList.add(v.y);
-        }
-        return returnList;
-    }
+        float windowWidth = Gdx.graphics.getWidth();
+        float windowHeight = Gdx.graphics.getHeight();
 
-    private PolygonRegion createPolygonRegion(){
-        EarClippingTriangulator ecp = new EarClippingTriangulator();
-        float[] verticesArray = convertToArray(vertices);
-        ShortArray s = ecp.computeTriangles(verticesArray);
-        short[] triangles = s.toArray();
-        PolygonRegion returnPolygonRegion = new PolygonRegion(darkRegion, verticesArray, triangles);
-        return returnPolygonRegion;
-    }
-
-    private RayCastCallback createCallback(){
-        return new RayCastCallback() {
-            @Override
-            public float reportRayFixture(Fixture fixture, Vector2 point, Vector2 normal, float fraction) {
-                if (fixture.getFilterData().categoryBits ==
-                        Constants.COLLISION_OBSTACLE){
-                    if (fraction < currentFraction) {
-                        currentFraction = fraction;
-                        collisionPoint.set(point);
-                        foundFixture =  true;
-                    }
-                }
-                return 1;
-            }
+        float[] corners = new float[]{
+                playerPosition.x - windowWidth/64, windowHeight/32 + playerPosition.y - windowHeight/64,        //Top left
+                playerPosition.x - windowWidth/64, playerPosition.y - windowHeight/64,                           //Bottom left
+                windowWidth/32+playerPosition.x - windowWidth/64, playerPosition.y - windowHeight/64,           //Bottom right
+                windowWidth/32 +playerPosition.x - windowWidth/64, windowHeight/32 + playerPosition.y - windowHeight / 64 //Top right
         };
-    }
 
-    private void initializeRays(){
-        rays = new Vector2[precision];
-        for(int i = 0; i<precision; i++){
-            rays[i] = new Vector2(1,1);
-            rays[i].setLength(length);
-            rays[i].setAngleRad(getDirection() - width / 2 + i * width / precision);
-            Vector2 end = new Vector2(rays[i]);
-            end.add(playerPosition);
-            endPoints.add(end);
+        collisionPoints.add(corners[0]);
+        collisionPoints.add(corners[1]);
+        for(int i = maxYIndex; i >= 0; i--) {
+            collisionPoints.add(endPoints.get(i).x);
+            collisionPoints.add(endPoints.get(i).y);
         }
-    }
+        for(int i =endPoints.size()-1; i >= maxYIndex; i--) {
+            collisionPoints.add(endPoints.get(i).x);
+            collisionPoints.add(endPoints.get(i).y);
+        }
 
-    private void initializeImages(){
-        darkRegion = new TextureRegion(darkness);
-        lightRegion = new Sprite(light);
+        for(float fl: corners)
+            collisionPoints.add(fl);
+        float[] collisionPointsArray = convertToArray(collisionPoints);
+
+        EarClippingTriangulator ecp = new EarClippingTriangulator();
+
+        float[] region1 = new float[collisionPointsArray.length];
+
+        for(int i = 0; i < region1.length; i++)
+            region1[i] = collisionPointsArray[i];
+
+
+        ShortArray s = ecp.computeTriangles(region1);
+        PolygonRegion darkness = new PolygonRegion(new TextureRegion(darkTexture), region1, s.toArray());
+        psb.draw(darkness,0,0);
     }
 
     private float[] convertToArray(ArrayList<Float> floatList){
@@ -193,5 +132,23 @@ public class Flashlight {
             i++;
         }
         return floatArray;
+    }
+
+    private RayCastCallback createCallback(){
+        RayCastCallback returnCallback = new RayCastCallback() {
+            @Override
+            public float reportRayFixture(Fixture fixture, Vector2 point, Vector2 normal, float fraction) {
+                if (fixture.getFilterData().categoryBits ==
+                        Constants.COLLISION_OBSTACLE){
+                    if (fraction < currentFraction) {
+                        currentFraction = fraction;
+                        collisionPoint.set(point);
+                    }
+                    foundFixture =  true;
+                }
+                return 1;
+            }
+        };
+        return returnCallback;
     }
 }
--- core/src/edu/chalmers/zombie/view/GameScreen.java
+++ core/src/edu/chalmers/zombie/view/GameScreen.java
@@ -45,7 +45,7 @@ public class GameScreen implements Screen{
     private World currentWorld;
     private OrthographicCamera camera;
     private OrthogonalTiledMapRenderer mapRenderer;
-    private Box2DDebugRenderer boxDebug;
+    //private Box2DDebugRenderer boxDebug;
     private MapController mapController;
     private float tileSize;
     private TiledMap tiledMap;
@@ -73,6 +73,7 @@ public class GameScreen implements Screen{
     private boolean foundFixture;
     private ArrayList<Float> collisionPoints = new ArrayList<Float>();
     private ArrayList<Float> corners = new ArrayList<Float>();
+    private Flashlight flashlight;
 
 
 
@@ -98,7 +99,7 @@ public class GameScreen implements Screen{
 
         //Starta rendrerare
         mapRenderer = new OrthogonalTiledMapRenderer(tiledMap,1/tileSize);
-        boxDebug = new Box2DDebugRenderer();
+       // boxDebug = new Box2DDebugRenderer();
 
 
 
@@ -125,6 +126,7 @@ public class GameScreen implements Screen{
         inputMultiplexer.addProcessor(inputProcessorOne);
         inputMultiplexer.addProcessor(inputProcessorTwo);
         Gdx.input.setInputProcessor(inputMultiplexer);
+        flashlight = new Flashlight(currentWorld);
 
     }
 
@@ -261,7 +263,7 @@ public class GameScreen implements Screen{
 
 
             //rita box2d debug
-            boxDebug.render(mapController.getWorld(), camera.combined);
+            //boxDebug.render(mapController.getWorld(), camera.combined);
             //render HUD
             String playerPos = "X: " + gameModel.getPlayer().getX() + ", Y: " + gameModel.getPlayer().getY();
             String playerHealth = "Health: " + gameModel.getPlayer().getLives();
@@ -280,144 +282,17 @@ public class GameScreen implements Screen{
 
 
             //------------------------------------------------------------------------
+
             PolygonSpriteBatch psb = new PolygonSpriteBatch();
             SpriteBatch sb = new SpriteBatch();
             sb.begin();
             psb.setProjectionMatrix(camera.combined);
             psb.begin();
-            Flashlight flashlight = new Flashlight(currentWorld);
             flashlight.draw(psb, sb);
             psb.end();
             sb.end();
-            /*
-            float direction = gameModel.getPlayer().getHand().getDirection() + Constants.PI/2;
-            playerPosition.set(gameModel.getPlayer().getX(), gameModel.getPlayer().getY());
-            RayCastCallback callback = new RayCastCallback() {
-                @Override
-                public float reportRayFixture(Fixture fixture, Vector2 point, Vector2 normal, float fraction) {
-                    if (fixture.getFilterData().categoryBits ==
-                            Constants.COLLISION_OBSTACLE){
-                        if (fraction < currentFraction) {
-                            currentFraction = fraction;
-                            coll.set(point);
-                        }
-                        foundFixture =  true;
-                    }
-                        return 1;
-                }
-            };
-            /* ----------------- TEST FLASHLIGHT -----------------*/
-            /*
-            float coneWidth = Constants.PI/4;
-            int numberOfRays = 100;
-            int coneLength = 8;
-            Vector2[] rays = new Vector2[numberOfRays];
-            Vector2 playerPosition =  new Vector2(gameModel.getPlayer().getX(), gameModel.getPlayer().getY());
-            ArrayList<Vector2> endPoints = new ArrayList<Vector2>();
-            for(int i = 0; i<numberOfRays; i++){
-                rays[i] = new Vector2(1,1);
-                rays[i].setLength(coneLength);
-                rays[i].setAngleRad(direction - coneWidth / 2 + i * coneWidth / numberOfRays);
-                Vector2 end = new Vector2(rays[i]);
-                end.add(playerPosition);
-                endPoints.add(end);
-
-            }
-            shapeRenderer.setAutoShapeType(true);
-            shapeRenderer.setProjectionMatrix(camera.combined);
-            shapeRenderer.setColor(Color.WHITE);
-            for (Vector2 line: rays){
-                currentFraction = 1337;
-                foundFixture = false;
-
-                currentWorld.rayCast(callback, new Vector2(gameModel.getPlayer().getX(), gameModel.getPlayer().getY()), new Vector2(line.x + gameModel.getPlayer().getX(), line.y + gameModel.getPlayer().getY()));
-                if (foundFixture){
-                    Vector2 temp = new Vector2(line);
-                    temp.add(playerPosition);
-                    int tempIndex = endPoints.indexOf(temp);
-                    endPoints.remove(temp);
-                    endPoints.add(tempIndex,new Vector2(coll));
-                }
-
-
-            }
-
-            collisionPoints.clear();
-            //addCorners(collisionPoints);
-
-
-                /*-----------------ERIK TESTAR--------------------*/
-            /*
-            endPoints.add(new Vector2(gameModel.getPlayer().getX(), gameModel.getPlayer().getY()));
-            float maxY = 0;
-            int maxYIndex = -1;
-            for(int i = 0; i<endPoints.size(); i++){
-                float currentY = endPoints.get(i).y;
-                if(currentY>maxY){
-                    maxY = currentY;
-                    maxYIndex = i;
-                }
-            }
-
-            float windowWidth = Gdx.graphics.getWidth();
-            float windowHeight = Gdx.graphics.getHeight();
-
-            float[] corners = new float[]{
-                    playerPosition.x - windowWidth/64, windowHeight/32 + playerPosition.y - windowHeight/64,        //Top left
-                    playerPosition.x - windowWidth/64, playerPosition.y - windowHeight/64,                           //Bottom left
-                    windowWidth/32+playerPosition.x - windowWidth/64, playerPosition.y - windowHeight/64,           //Bottom right
-                    windowWidth/32 +playerPosition.x - windowWidth/64, windowHeight/32 + playerPosition.y - windowHeight / 64 //Top right
-            };
-
-            collisionPoints.add(corners[0]);
-            collisionPoints.add(corners[1]);
-            for(int i = maxYIndex; i >= 0; i--) {
-                collisionPoints.add(endPoints.get(i).x);
-                collisionPoints.add(endPoints.get(i).y);
-            }
-            for(int i =endPoints.size()-1; i >= maxYIndex; i--) {
-                collisionPoints.add(endPoints.get(i).x);
-                collisionPoints.add(endPoints.get(i).y);
-            }
-           // collisionPoints.add(endPoints.get(maxYIndex).x);
-            //collisionPoints.add(endPoints.get(maxYIndex).y);
-            for(float fl: corners)
-                collisionPoints.add(fl);
-            float[] collisionPointsArray = convertToArray(collisionPoints);
-
-
-            //---------------------------------------------------------------------
-
-            shapeRenderer.setProjectionMatrix(camera.combined);
-
-
-            EarClippingTriangulator ecp = new EarClippingTriangulator();
-
-
-            //ShortArray s = ecp.computeTriangles(region1);
-
-            float[] region1 = new float[collisionPointsArray.length];
-
-            for(int i = 0; i < region1.length; i++)
-                region1[i] = collisionPointsArray[i];
-
-
-           ShortArray s = ecp.computeTriangles(region1);
-            PolygonRegion darkness = new PolygonRegion(new TextureRegion(tex), region1, s.toArray());
-            shapeRenderer.setAutoShapeType(true);
-            PolygonSpriteBatch psb = new PolygonSpriteBatch();
-
-            psb.setProjectionMatrix(camera.combined);
-            batchHUD.begin();
-            Sprite lightSprite = new Sprite(light);
-            lightSprite.setAlpha(0.15f);
-            lightSprite.draw(batchHUD);
-            batchHUD.end();
-            psb.begin();
-            //psb.draw(darkness1, 0, 0);
-            psb.draw(darkness,0,0);
-            psb.end();
-
+            psb.dispose();
+            sb.dispose();
             /*---------------- END TEST -------------------------*/
          /*--------------------------TESTA PATH FINDING------------------------------------*/
 
@@ -429,7 +304,7 @@ public class GameScreen implements Screen{
         /*-----------------SLUTTESTAT---------------------*/
 
             //rita box2d debug
-            boxDebug.render(mapController.getWorld(), camera.combined);
+            //boxDebug.render(mapController.getWorld(), camera.combined);
         //render HUD
         playerPos = "X: " + gameModel.getPlayer().getX() + ", Y: " + gameModel.getPlayer().getY();
         playerHealth = "Health: " + gameModel.getPlayer().getLives();
@@ -441,7 +316,6 @@ public class GameScreen implements Screen{
         bitmapFont.draw(batchHUD, playerPos, 10, Gdx.graphics.getHeight()-40);
         batchHUD.end();
         }
-
     }
 
     /**
@@ -525,6 +399,7 @@ public class GameScreen implements Screen{
         currentWorld.dispose();
         batchHUD.dispose();
         shapeRenderer.dispose();
+
     }
 
     /**
