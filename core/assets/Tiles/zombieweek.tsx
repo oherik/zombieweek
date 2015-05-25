b568985fb633dc23386afaeff64d2d7c4ef46b5e
oherik <erik.ohrn@outlook.com>
2015-05-25 12:14:36 +0200

    Added custom zombie tiles

---
+++ core/assets/Tiles/zombieweek.tsx
@@ -0,0 +1,86 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="mesh" tilewidth="32" tileheight="32">
+ <tile id="0">
+  <properties>
+   <property name="collision_zombie" value="true"/>
+   <property name="water" value="true"/>
+  </properties>
+  <image width="32" height="32" source="../Tiles/mesh_blue.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="collision_zombie" value="true"/>
+   <property name="door" value="2"/>
+  </properties>
+  <image width="32" height="32" source="../Tiles/mesh_green.png"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="collision_zombie" value="true"/>
+   <property name="sneak" value="true"/>
+  </properties>
+  <image width="32" height="32" source="../Tiles/mesh_orange.png"/>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="zombie_spawn" value="default"/>
+  </properties>
+  <image width="32" height="32" source="../Tiles/mesh_purple.png"/>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="collision_all" value="true"/>
+   <property name="collision_zombie" value="true"/>
+  </properties>
+  <image width="32" height="32" source="../Tiles/mesh_red.png"/>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="collision_zombie" value="true"/>
+   <property name="door" value="0"/>
+  </properties>
+  <image width="32" height="32" source="../Tiles/mesh_yellow.png"/>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="player_spawn" value="true"/>
+  </properties>
+  <image width="32" height="32" source="../Tiles/mesh_teal.png"/>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="player_return" value=""/>
+  </properties>
+  <image width="32" height="32" source="../Tiles/mesh_pink.png"/>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="zombie_spawn" value="boss"/>
+  </properties>
+  <image width="32" height="32" source="../Tiles/zombie_boss.png"/>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="zombie_spawn" value="d"/>
+  </properties>
+  <image width="32" height="32" source="../Tiles/zombie_d.png"/>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="zombie_spawn" value="i"/>
+  </properties>
+  <image width="32" height="32" source="../Tiles/zombie_i.png"/>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="zombie_spawn" value="it"/>
+  </properties>
+  <image width="32" height="32" source="../Tiles/zombie_it.png"/>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="zombie_spawn" value="z"/>
+  </properties>
+  <image width="32" height="32" source="../Tiles/zombie_z.png"/>
+ </tile>
+</tileset>
