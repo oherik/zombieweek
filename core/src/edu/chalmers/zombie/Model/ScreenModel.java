fd99fd2be9da563986c9cdb4cc8b983a12660ec1
Tobias <toobew@users.noreply.github.com>
2015-05-28 13:28:49 +0200

    Created Screen model

--- core/src/edu/chalmers/zombie/Model/ScreenModel.java
+++ core/src/edu/chalmers/zombie/Model/ScreenModel.java
@@ -1,7 +1,50 @@
 package edu.chalmers.zombie.model;
 
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
 /**
  * Created by Tobias on 15-05-28.
  */
 public class ScreenModel {
+
+    private Stage pauseStage;
+    private Stage gameOverStage;
+    private Stage soundAndSettingStage;
+    private Stage nextLevelStage;
+    public ScreenModel(){}
+
+    public Stage getSoundAndSettingStage() {
+        return soundAndSettingStage;
+    }
+
+    public void setSoundAndSettingStage(Stage soundAndSettingStage) {
+        this.soundAndSettingStage = soundAndSettingStage;
+    }
+
+    public Stage getPauseStage() {
+        return pauseStage;
+    }
+
+    public void setPauseStage(Stage pauseStage) {
+        this.pauseStage = pauseStage;
+    }
+
+    public Stage getGameOverStage() {
+        return gameOverStage;
+    }
+
+    public void setGameOverStage(Stage gameOverStage) {
+        this.gameOverStage = gameOverStage;
+    }
+
+    public Stage getNextLevelStage() {
+        return nextLevelStage;
+    }
+
+    public void setNextLevelStage(Stage nextLevelStage) {
+        this.nextLevelStage = nextLevelStage;
+    }
+
+
+
 }
