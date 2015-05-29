19c5522c64d43044480921d2a7c4ed1ef7610c49
Tobias <toobew@users.noreply.github.com>
2015-05-29 19:52:14 +0200

    Added initializer

--- core/src/edu/chalmers/zombie/Controller/AudioController.java
+++ core/src/edu/chalmers/zombie/Controller/AudioController.java
@@ -2,13 +2,26 @@ package edu.chalmers.zombie.controller;
 
 import edu.chalmers.zombie.adapter.ZWSound;
 import edu.chalmers.zombie.model.GameModel;
+import edu.chalmers.zombie.utils.ResourceManager;
 
 /**
- * Controller for audio
+ * Controller for audio and sound effects
  *
  * Created by Tobias on 15-05-21.
  */
 public class AudioController {
+
+    /**
+     * Initializes all sound effects and music used in the application
+     */
+    public static void initializeSounds(){
+        ResourceManager res = GameModel.getInstance().res;
+        res.loadSound("throw", "core/assets/Audio/Sound_effects/throw_book.mp3");
+        res.loadSound("menu_hover", "core/assets/Audio/Sound_effects/menu_hover.mp3");
+        res.loadSound("zombie_hit","core/assets/Audio/Sound_effects/zombie_hit.mp3");
+        res.loadSound("pick_up_book","core/assets/Audio/Sound_effects/pick_up_book.mp3");
+        res.loadSound("zombie_sleeping","core/assets/Audio/Sound_effects/zombie_sleeping.mp3");
+    }
 
 
     /**
@@ -23,7 +36,7 @@ public class AudioController {
     }
 
     /**
-     * Toggle sound
+     * Toggle sound ON/OFF
      */
     public static void toggleSound(){
         GameModel gameModel = GameModel.getInstance();
