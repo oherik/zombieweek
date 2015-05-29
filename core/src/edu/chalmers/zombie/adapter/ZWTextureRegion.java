9cf45ea65a8611cc55acea252382d11b5e4f7ca0
Tobias <toobew@users.noreply.github.com>
2015-05-29 15:19:29 +0200

    Added wrapper methods

--- core/src/edu/chalmers/zombie/adapter/ZWTextureRegion.java
+++ core/src/edu/chalmers/zombie/adapter/ZWTextureRegion.java
@@ -1,7 +1,25 @@
 package edu.chalmers.zombie.adapter;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
 /**
  * Created by Tobias on 15-05-29.
  */
 public class ZWTextureRegion {
+    private TextureRegion textureRegion;
+
+    public ZWTextureRegion(){
+        this.textureRegion = new TextureRegion();
+    }
+
+
+    public ZWTexture getZWTexture(){return new ZWTexture(this.textureRegion.getTexture());}
+
+    public ZWTexture consumePixmap(){
+        return  new ZWTexture(new ZWPixmap(this.textureRegion.getTexture().getTextureData().consumePixmap()));
+    }
+
+
+
 }
