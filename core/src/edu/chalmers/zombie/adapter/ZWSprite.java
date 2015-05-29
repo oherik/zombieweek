dfd6d1772d767f6dfd807384db4e2534bcecfc6e
Erik Öhrn <erik.ohrn@outlook.com>
2015-05-29 16:02:23 +0200

    Started cleaning up entity

--- core/src/edu/chalmers/zombie/adapter/ZWSprite.java
+++ core/src/edu/chalmers/zombie/adapter/ZWSprite.java
@@ -12,6 +12,9 @@ public class ZWSprite {
     public ZWSprite(Texture texture){
         sprite = new Sprite(texture);
     }
+    public ZWSprite(ZWTexture texture){
+        sprite = new Sprite(texture.getTexture());
+    }
     public ZWSprite(Sprite sprite){
         this.sprite = sprite;
     }
@@ -24,4 +27,30 @@ public class ZWSprite {
     public void setAlpha(float alpha){
         sprite.setAlpha(alpha);
     }
+    public float getWidth(){
+        return sprite.getWidth();
+    }
+    public float getHeight(){
+        return sprite.getHeight();
+    }
+    public void dispose(){
+        sprite.getTexture().dispose();
+    }
+
+    public void setOrigin(float x, float y){
+        sprite.setOrigin(x, y);
+}
+    public void setRotation(float degrees){
+        sprite.setRotation(degrees);
+    }
+
+    public void setY(float y){
+        sprite.setY(y);
+    }
+
+    public void setX(float x){
+        sprite.setX(x);
+    }
+
+
 }
