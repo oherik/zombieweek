8ac0a03d65921d7febcbacbbb240aba3c3c7cf53
Erik Öhrn <erik.ohrn@outlook.com>
2015-05-29 16:32:32 +0200

    Cleaned up the actor controllers

--- core/src/edu/chalmers/zombie/Controller/PlayerController.java
+++ core/src/edu/chalmers/zombie/Controller/PlayerController.java
@@ -7,7 +7,9 @@ import edu.chalmers.zombie.model.GameModel;
 import edu.chalmers.zombie.utils.Constants;
 import edu.chalmers.zombie.utils.Direction;
 import edu.chalmers.zombie.utils.PotionType;
+import edu.chalmers.zombie.utils.ResourceManager;
 
+import java.awt.*;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -82,6 +84,100 @@ public class PlayerController {
         }
         public void run() {
             GameModel.getInstance().getPlayer().setLegPower(speed);
+        }
+    }
+
+    /**
+     * @return  The current player
+     */
+    public static Player getPlayer(){
+        return GameModel.getInstance().getPlayer();
+    }
+    /**
+     * Sets the current player
+     * @param player The new player
+     */
+    public static void setPlayer(Player player){
+        GameModel.getInstance().setPlayer(player);
+    }
+
+    /**
+     * Creates a new player and sets it in the game model.
+     * @return  The newly created player
+     */
+    public static Player createNewPlayer(){
+        MapController mapController = new MapController();
+
+        GameModel gameModel = GameModel.getInstance();
+        ResourceManager res = gameModel.res;
+
+        Point position = mapController.getPlayerBufferPosition();
+        Player player;
+        try {
+            player = new Player(res.getTexture("emilia"), gameModel.getRoom().getWorld().getWorld(), position.x, position.y);
+        }catch (NullPointerException e){
+            System.err.println("No buffered position found. Placing player at room spawn.");
+            position = GameModel.getInstance().getRoom().getPlayerSpawn();
+            player = new Player(res.getTexture("emilia"),gameModel.getRoom().getWorld().getWorld(), position.x, position.y);
+        }
+        setPlayer(player); //TODO test);
+        return player;
+    }
+
+    /**
+     * Increse the number of sneak tiles the player is touching by one. The player will start to sneak.
+     * @param player    The player
+     */
+    public static void increaseSneakTilesTouching(Player player){
+        if(player.getSneakTilesTouching()<0) {
+            player.setSneakTilesTouching(0);        //Negative numbers for tiles youching wouldn't make sense
+        }
+        player.setSneakTilesTouching(player.getSneakTilesTouching() + 1);
+        //TODO add more sneak stuff
+        player.setHidden(true); //TODO Nödvändigt? Zombien kommer ändå inte kunna hitta till spelaren
+        EntityController.setFriction(player, Constants.PLAYER_FRICTION_SNEAK, Constants.PLAYER_FRICTION_SNEAK);  //TODO onödigt att göra varje gång?
+    }
+
+    /**
+     * Decrease the number of sneak tiles the player is touching by one. The player will stop to sneak if the total number of sneak tiles it's touching is 0.
+     * @param player The player
+     */
+    public static void decreaseSneakTilesTouching(Player player){
+        player.setSneakTilesTouching(player.getSneakTilesTouching() - 1);
+        if(player.getSneakTilesTouching()<1){
+            //TODO Sluta sneaka
+            player.setHidden(false);
+            EntityController.setFriction(player, Constants.PLAYER_FRICTION_DEFAULT, Constants.PLAYER_FRICTION_DEFAULT);
+            if(player.getSneakTilesTouching()<0) {
+                player.setSneakTilesTouching(0);        //Negative numbers for tiles touching wouldn't make sense
+            }
+        }
+    }
+
+    /**
+     * Increse the number of water tiles the player is touching by one. The player will slow down.
+     * @param player    The player
+     */
+    public static void increaseWaterTilesTouching(Player player){
+        if(player.getWaterTilesTouching()<0) {
+            player.setWaterTilesTouching(0);
+        }
+        player.setWaterTilesTouching(player.getWaterTilesTouching() + 1);
+        //TODO add water stuff
+        EntityController.setFriction(player, Constants.PLAYER_FRICTION_WATER, Constants.PLAYER_FRICTION_WATER);
+    }
+
+    /**
+     * Decrease the number of water tiles the player is touching by one. The player will get out of the water if the total number of water tiles it's touching is 0.
+     * @param player The player
+     */
+    public static void decreaseWaterTilesTouching(Player player){
+        player.setWaterTilesTouching(player.getWaterTilesTouching() - 1);
+        if(player.getWaterTilesTouching()<1){
+            EntityController.setFriction(player, Constants.PLAYER_FRICTION_DEFAULT, Constants.PLAYER_FRICTION_DEFAULT);
+            if(player.getWaterTilesTouching()<0) {
+                player.setWaterTilesTouching(0);
+            }
         }
     }
 }
---
+++ core/src/edu/chalmers/zombie/Controller/ProjectileController.java
@@ -0,0 +1,70 @@
+package edu.chalmers.zombie.controller;
+
+import edu.chalmers.zombie.adapter.Book;
+import edu.chalmers.zombie.adapter.Player;
+import edu.chalmers.zombie.adapter.Zombie;
+import edu.chalmers.zombie.model.GameModel;
+import edu.chalmers.zombie.model.Room;
+import edu.chalmers.zombie.utils.Constants;
+
+/**
+ * Created by Erik on 2015-05-29.
+ */
+public class ProjectileController {
+
+    /**
+     * Performs the calculations necessary for the book to hit the ground. It calls other methods to set the collision detection to players and other books as well and to apply friction.
+     * @param book  The book
+     */
+    public static void hitGround(Book book){
+        book.setOnGround(true);
+        //TODO g?ra boken mindre, l?gga till ljud etc
+        short maskBits = Constants.COLLISION_OBSTACLE | Constants.COLLISION_ENTITY | Constants.COLLISION_WATER | Constants.COLLISION_ACTOR_OBSTACLE;
+        EntityController.setMaskBits(book, maskBits);
+        EntityController.setFriction(book, 4f, 3f);
+    }
+
+    public static void spawnStillBook(Room room, int x, int y){
+        Book book = new Book(x,y,room);
+        hitGround(book);
+        GameModel.getInstance().addBook(book);
+    }
+
+
+
+    /**
+     * A zombie gets hit by a book
+     * @param z The zombie
+     * @param b The book
+     */
+    public static void applyHit(Zombie z, Book b){
+
+        z.setIsAggressive(true);
+        if(!b.isOnGround()) {
+            int damage = b.getDamage();
+            z.decHp(damage);
+            if (z.getHP() <= 0) {
+                ZombieController.knockOut(z);
+            }
+            EntityController.knockBack(b, z, damage / 10);
+            hitGround(b);
+            // GameModel.getInstance().addEntityToRemove(b);
+            //b.markForRemoval();
+            AudioController.playSound(GameModel.getInstance().res.getSound("zombie_hit"));
+        }
+    }
+
+    /**
+     * A player picks up a book
+     * @param p The player
+     * @param b The book
+     */
+    public static void pickUp(Player p, Book b){
+        GameModel.getInstance().addEntityToRemove(GameModel.getInstance().getRoom(),b); //TODO beh?vs b?da dessa?
+        b.markForRemoval();             //TODO beh?vs b?da dessa?
+        p.increaseAmmunition();
+        AudioController.playSound(GameModel.getInstance().res.getSound("pick_up_book"));
+    }
+
+
+}
