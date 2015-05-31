83aa86280a04ce5d94404c79a9a9754730597205
Erik Öhrn <erik.ohrn@outlook.com>
2015-05-31 13:25:03 +0200

    Fixed final fields not being final

--- core/src/edu/chalmers/zombie/utils/Constants.java
+++ core/src/edu/chalmers/zombie/utils/Constants.java
@@ -55,9 +55,9 @@ public class Constants {
     public static final float SQRT_2 = 1.4142f;
 
     //Potions
-    public static int POTION_HEALTH_AMOUNT = 50;
-    public static float POTION_SPEED_SCALE = 3f;
-    public static int POTION_SPEED_TIME_MILLIS = 5000;
+    public static final int POTION_HEALTH_AMOUNT = 50;
+    public static final float POTION_SPEED_SCALE = 3f;
+    public static final int POTION_SPEED_TIME_MILLIS = 5000;
 
 
 }
