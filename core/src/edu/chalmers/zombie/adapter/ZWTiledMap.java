79727488928627460d34712e61e25e722ceb7263
Erik Öhrn <erik.ohrn@outlook.com>
2015-05-31 13:53:46 +0200

    Fixed bad typecasts

--- core/src/edu/chalmers/zombie/adapter/ZWTiledMap.java
+++ core/src/edu/chalmers/zombie/adapter/ZWTiledMap.java
@@ -1,5 +1,6 @@
 package edu.chalmers.zombie.adapter;
 
+import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
@@ -23,28 +24,39 @@ public class ZWTiledMap {
     }
 
     public boolean tileIsEmpty(String layer, int col, int row) {
-        TiledMapTileLayer  tiledLayer = (TiledMapTileLayer) tiledMap.getLayers().get(layer);
-        TiledMapTileLayer.Cell currentCell = tiledLayer.getCell(col, row);
-        return(currentCell==null||currentCell.getTile()==null);
+        MapLayer mapLayer = tiledMap.getLayers().get(layer);
+        if(mapLayer instanceof TiledMapTileLayer) {
+            TiledMapTileLayer  tiledLayer = (TiledMapTileLayer) mapLayer;
+            TiledMapTileLayer.Cell currentCell = tiledLayer.getCell(col, row);
+            return(currentCell==null||currentCell.getTile()==null);
+        }
+        return true;
+
     }
 
     public boolean hasProperty(String layer, int col, int row, String property){
-        TiledMapTileLayer  tiledLayer = (TiledMapTileLayer) tiledMap.getLayers().get(layer);
-        TiledMapTileLayer.Cell currentCell = tiledLayer.getCell(col, row);
-        return (!tileIsEmpty(layer,col, row) && currentCell.getTile().getProperties().get(property) != null);
+        MapLayer mapLayer = tiledMap.getLayers().get(layer);
+        if(mapLayer instanceof TiledMapTileLayer) {
+             TiledMapTileLayer  tiledLayer = (TiledMapTileLayer) mapLayer ;
+             TiledMapTileLayer.Cell currentCell = tiledLayer.getCell(col, row);
+             return (!tileIsEmpty(layer,col, row) && currentCell.getTile().getProperties().get(property) != null);
+        }
+        return false;
     }
 
     public Object getProperty(String layer, int col, int row, String propertyName){
-        TiledMapTileLayer  tiledLayer = (TiledMapTileLayer) tiledMap.getLayers().get(layer);
-        TiledMapTileLayer.Cell currentCell = tiledLayer.getCell(col, row);
-        if(hasProperty(layer, col, row,propertyName))
-            return currentCell.getTile().getProperties().get(propertyName);
-        else
-            return null;
+        MapLayer mapLayer = tiledMap.getLayers().get(layer);
+        if(mapLayer instanceof TiledMapTileLayer) {
+            TiledMapTileLayer tiledLayer = (TiledMapTileLayer) mapLayer;
+            TiledMapTileLayer.Cell currentCell = tiledLayer.getCell(col, row);
+            if (hasProperty(layer, col, row, propertyName))
+                return currentCell.getTile().getProperties().get(propertyName);
+        }
+        return null;
     }
 
     public void setVisibible(String layer, boolean visible){
-        TiledMapTileLayer  mapLayer = (TiledMapTileLayer) tiledMap.getLayers().get(layer);
+        MapLayer mapLayer =  tiledMap.getLayers().get(layer);
         mapLayer.setVisible(visible);
     }
 
