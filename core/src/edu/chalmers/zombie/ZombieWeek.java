86b97a7ba656b608556becb5b6888aea4dc08be9
Tobias <toobew@users.noreply.github.com>
2015-05-31 20:31:35 +0200

    Loads game

--- core/src/edu/chalmers/zombie/ZombieWeek.java
+++ core/src/edu/chalmers/zombie/ZombieWeek.java
@@ -1,14 +1,15 @@
 package edu.chalmers.zombie;
 
 import com.badlogic.gdx.Game;
-import edu.chalmers.zombie.controller.MapController;
+import edu.chalmers.zombie.utils.SaveLoadGame;
 import edu.chalmers.zombie.view.GameScreen;
-import edu.chalmers.zombie.view.MainMenuScreen;
 
 public class ZombieWeek extends Game {
 
 	public void create () {
         //setScreen(new MainMenuScreen()); //Main menu screen
+        SaveLoadGame saveLoadGame = new SaveLoadGame();
+        saveLoadGame.loadGame();
         setScreen(new GameScreen()); //Game screen
 	}
 
